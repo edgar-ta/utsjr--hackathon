8cb31935d81b838dc2a19f33ec3f8c32d7f318af
“ShaguittoScot” <arields.ti23@utsjr.edu.mx>
2024-08-26 21:37:21 -0600

    Inicio de sesion

---
+++ routes/userRoutes.js
@@ -0,0 +1,32 @@
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Ruta para iniciar sesión
+router.post('/login', async (req, res) => {
+    const { correo, contrasena } = req.body;
+
+    if (!correo || !contrasena) {
+        return res.status(400).json({ message: 'Correo y contraseña son requeridos' });
+    }
+
+    try {
+        const usuariobd = new UsuarioBD();
+        const usuario = await usuariobd.obtenerUsuarioPorCorreo(correo);
+
+        if (usuario && await bcrypt.compare(contrasena, usuario.contrasena)) {
+            const token = jwt.sign(
+                { id: usuario.id, nombre: usuario.nombre, correo: usuario.correo },
+                secret,
+                { expiresIn: '1h' }
+            );
+            res.cookie('token', token, { httpOnly: true, secure: false });
+            res.redirect('/calculadora');
+        } else {
+            res.status(401).json({ message: 'Correo o contraseña incorrectos' });
+        }
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        res.status(500).json({ message: 'Error en el servidor' });
+    }
+});
